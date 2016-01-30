314b72851571ef64f43dac844973aa64e94f1aa2
Jun Cai <cai.jun@husky.neu.edu>
2016-01-30 00:47:11 -0500

    work on the R script

--- hw2/Cai_Jun_A2/myR.R
+++ hw2/Cai_Jun_A2/myR.R
@@ -1,13 +1,26 @@
 setwd("./output")
 res <- read.csv(file="part-r-00000", head=FALSE)
 names(res) <- c("month", "carrier", "totalFl", "meanPrice")
-# Start of EXTRA work
+# attach(res)
+# sorted_res <- res[order(month, -meanPrice),]
+# sorted_res <- sorted_res[!is.na(totalFl)]
+
+# find the top 10 carriers with most flights
 require(plyr)
-team_msalary <- ddply(res,~teamID,summarise,meanS=mean(salary))
-team_mhr <- ddply(batting,~teamID,summarise,meanH=mean(HR))
-extra_res1 <- merge(team_mhr, team_msalary, by="teamID")
-qplot(extra_res1$meanS, extra_res1$meanH)
+carrier_flnum <- ddply(res,~carrier,summarise,totalFL=mean(totalFl))
+attach(carrier_flnum)
+sorted_carrier <- carrier_flnum[order(totalFL, decreasing=TRUE),]
+top_carriers <- sorted_carrier[1:10]
+top_carriers <- top_carriers$carrier
+# sorted_res <- sorted_res[carrier %in% top_carriers,]
 
-
+# loop through top carriers and plot the mean prices for each month
+for (i in 1:10) {
+	c_carrier <- top_carriers[i]
+	c_df <- res[carrier == c_carrier,]
+	attach(c_df)
+	c_sorted <- c_df[order(month)]
+	plot(c_sorted$month, c_sorted$meanPrice)
+}
 
 
