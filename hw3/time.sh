66eff826d4d51cb39761b5758e444b82f801d4a8
vikasboddu <boddu.v@husky.neu.edu>
2016-02-06 20:57:10 -0500

    Update time.sh

--- hw3/time.sh
+++ hw3/time.sh
@@ -6,13 +6,33 @@ strindex() {
   [[ $x = $1 ]] && echo -1 || echo ${#x}
 }
 
+emr() { 
+  path="$(find . -name "cid.temp")"
+  cid=$(cat "$path")
+
+  readydatetimetext=$(aws emr describe-cluster --cluster-id "$cid" | grep -oh '^[[:space:]]\{4\}"ReadyDateTime": ')
+  enddatetimetext=$(aws emr describe-cluster --cluster-id "$cid" | grep -oh '^[[:space:]]\{6\}"EndDateTime": ')
+  
+  delimiter=":"
+  readydatelengthstart=$(strindex "$readydatetimetext" "$delimiter")
+  enddatetimelengthstart=$(strindex "$enddatetimetext" "$delimiter")
+  
+  readydatelengthfin=${#readydatetimetext}
+  enddatetimelengthfin=${#enddatetimetext}
+  
+  readydatetime=${readydatetimetext:$((readydatelengthstart+2)):$((readydatelengthfin-1))}
+  enddatetimetext=${readydatetimetext:$((enddatetimelengthstart+2)):enddatetimelength}
+  
+  elapsedtime=$((elapseddatetimes-readydatetime))
+  echo "$elapsedtime"
+}
+
 size=$2
 pre=$(date +%s)
 
 ./$1
 
 post=$(date +%s)
-actual=$((post-pre))
 
 realname=$1
 desiredlength=${#realname}
@@ -24,5 +44,15 @@ executiontype=${outputname:9:offset}
 interprogramtype=${outputname:$((offset+1)):desiredlength}
 programtype=$interprogramtype$size
 
-echo "${executiontype}, ${actual}, ${programtype}" >> my_file.csv
+emr="emr"
+containsemr=$(strindex $executiontype "$emr")
+
+if [ $containsemr -eq -1 ]
+then
+  actualtime=$((post-pre))
+else
+  actualtime=$(emr)
+fi
+
+echo "${executiontype}, ${actualtime}, ${programtype}" >> my_file.csv
 
