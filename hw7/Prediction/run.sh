0322ae4e11876f193b6cd50027ee84c81313b4c8
Juncai <cai.jun@husky.neu.edu>
2016-03-20 00:00:25 -0400

    doc

--- hw7/Prediction/run.sh
+++ hw7/Prediction/run.sh
@@ -2,10 +2,12 @@ cd train
 ./run.sh -prepare
 ./run.sh -data
 ./run.sh -pd
+cp output/part-r-00000 /tmp/final.rf
 cd ..
 
 cd test
+gradle jar
 ./run.sh -data
 ./run.sh -pd
-# ./run.sh -clean
+./run.sh -clean
 cd ..
--- hw7/Routing/run.sh
+++ hw7/Routing/run.sh
@@ -1,11 +1,19 @@
 cd train
-# ./run.sh -prepare
+./run.sh -prepare
 ./.run.sh -data
 ./run.sh -pd
+cp output/part-r-00000 /tmp/final.rf
 cd ..
 
-cd test
+cd predict
 ./run.sh -data
 ./run.sh -pd
-# ./run.sh -clean
+cp output/part-r-00000 ${ROUTING_PREDICTION_DIR}/prediction.csv
+cd ..
+
+cd query
+./run.sh -data
+./run.sh -pd
+./run.sh -clean
+Rcript totalDuration.R output ${ROUTING_VALIDATE_DIR}
 cd ..
