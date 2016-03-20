1e73b53b52c38a45af08e3077997a3cb2c7dab4b
vikasboddu <boddu.v@husky.neu.edu>
2016-03-19 23:55:22 -0400

    Update combineRF.R

--- hw7/Routing/train/combineRF.R
+++ hw7/Routing/train/combineRF.R
@@ -2,7 +2,6 @@ library(randomForest)
 library(methods)
 
 args = commandArgs(trailingOnly=TRUE)
-#input <- args[1]
 output <- args[1]
 
 # credit: joran's post: http://stackoverflow.com/questions/19170130/combining-random-forests-built-with-different-training-sets-in-r
@@ -88,19 +87,15 @@ getForest <- function(path) {
 }
 
 input_path <- "./"
-#filenames <- dir(input_path, pattern="OTP_prediction_forest_*", full.names=TRUE)
 filenames <- list.files(input_path, pattern="OTP_prediction_forest_*", full.names=TRUE)
-#filenames <- list.files(input_path, pattern="OTP_test_*", full.names=TRUE)
 if (length(filenames) > 0) {
 	forest <- getForest(filenames[1])
 	if (length(filenames) > 1) {
 		for (i in 2:length(filenames)) {
 			nextForest <- getForest(filenames[i])
-#			forest <- combine(forest, nextForest)
 			forest <- my_combine(forest, nextForest)
 		}
 	}
 	rfString <- rawToChar(serialize(forest, NULL, ascii=TRUE))
-	#write(rfString, file = "/tmp/OTP_prediction_final.rf")
 	write(rfString, file = output)
 }
