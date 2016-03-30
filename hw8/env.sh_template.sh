e5f3ff4919c1dc0b3e33499ddbb4ec1f49a48b60
Xi Wang <emmawangxi03@gmail.com>
2016-03-30 11:09:38 -0400

    add env.sh_template file

---
+++ hw8/env.sh_template.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# author: Xi Wang
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
+export AWS_DEFAULT_REGION=us-east-1
+
+# please create a key pair
+export EC2_KEY_PAIR_NAME=
+export EC2_PRIVATE_KEY_PATH=
+
+# security group information
+export EC2_SECURITY_GROUP=
+
+# s3 bucket name
+export INPUT_BUCKET= cs6240sp16
+export OUTPUT_BUCKET=
+
+# AMI
+export IMAGE_ID=ami-8fcee4e5
+
+# ec2 username
+export EC2_USERNAME=ec2-user
