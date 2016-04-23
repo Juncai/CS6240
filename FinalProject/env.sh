0fd912435eb7954686e19d5c006a2a3f88f66fff
Alexwings <wang.xinyuan@husky.neu.edu>
2016-04-23 19:11:42 -0400

    Modify scripts

--- FinalProject/env.sh
+++ FinalProject/env.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # author: Xi Wang
-export AWS_ACCESS_KEY_ID=AKIAJSXCXRGQET7DDGMQ
-export AWS_SECRET_ACCESS_KEY=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
+export AWS_ACCESS_KEY_ID=AKIAIPJFSKFCPSVYBDDA
+export AWS_SECRET_ACCESS_KEY=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
 export AWS_DEFAULT_REGION=us-east-1
 
 # please create a key pair
-export EC2_KEY_PAIR_NAME=alexwang_ec2
-export EC2_PRIVATE_KEY_PATH=/home/alexwang/key/alexwang_ec2.pem
+export EC2_KEY_PAIR_NAME=test_key
+export EC2_PRIVATE_KEY_PATH=/home/alexwang/key/test_key.pem
 
 # security group information
 export EC2_SECURITY_GROUP=default
