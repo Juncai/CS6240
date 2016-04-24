b7de19ded270e4287ca13f238aa23f56183cf2a1
Juncai <cai.jun@husky.neu.edu>
2016-04-24 16:28:52 -0400

    retry compile

--- FinalProject/run.sh
+++
@@ -1,9 +0,0 @@
-./compile.sh
-
-./start-cluster.sh $1
-
-./sort.sh "dry buld temp" s3://cs6240sp16/climate s3://juncai001/output
-
-./get_logs.sh > /dev/null 2>&1 &
-
-./stop-cluster.sh > /dev/null 2>&1 &
